820b96264585a94db2e1bda9569e9f9c98168419
Filip Talaga <talaga.filip@gmail.com>
2019-09-16 19:01:56 +0200

    Moved update player code into game module

--- src/app/game.js
+++ src/app/game.js
@@ -1,3 +1,10 @@
+function willCollide(player, obstacle) {
+    const vertical = player.y + player.height > obstacle.y && player.y < obstacle.y + obstacle.height;
+    const horizontal = player.x + player.width > obstacle.x && player.x < obstacle.x + obstacle.width;
+
+    return vertical && horizontal;
+}
+
 function makeGame() {
     const player = {
         x: 0,
@@ -17,9 +24,32 @@ function makeGame() {
         height: 30,
     };
 
+    const updatePlayerPosition = distance => {
+        if (player.movingUp) {
+            const futurePlayer = { ...player, y: player.y - distance };
+            player.y = willCollide(futurePlayer, obstacle) ? obstacle.y + obstacle.height : futurePlayer.y;
+        }
+
+        if (player.movingDown) {
+            const futurePlayer = { ...player, y: player.y + distance };
+            player.y = willCollide(futurePlayer, obstacle) ? obstacle.y - player.height : futurePlayer.y;
+        }
+
+        if (player.movingLeft) {
+            const futurePlayer = { ...player, x: player.x - distance };
+            player.x = willCollide(futurePlayer, obstacle) ? obstacle.x + obstacle.width : futurePlayer.x;
+        }
+
+        if (player.movingRight) {
+            const futurePlayer = { ...player, x: player.x + distance };
+            player.x = willCollide(futurePlayer, obstacle) ? obstacle.x - player.width : futurePlayer.x;
+        }
+    }
+
     return {
         player,
-        obstacle
+        obstacle,
+        updatePlayerPosition,
     }
 }
 
