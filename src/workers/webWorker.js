1e2cf6e462e63d7bab831b77a12fce9ae7c5b258
Filip Talaga <talaga.filip@gmail.com>
2019-09-16 16:17:32 +0200

    Removed unnecessary white space

--- src/workers/webWorker.js
+++ src/workers/webWorker.js
@@ -1,17 +1,13 @@
 function makeWebWorker(worker, commands) {
     // Stringify worker function and commands definition
     const code = worker.toString();
-    const commandsObjectLine = `const commands = ${JSON.stringify(commands)};`;
+    const commandsObjectLine = `const commands=${JSON.stringify(commands)};`;
 
     // Find the beginning of the function scope
     const codeScopeStart = code.indexOf('{') + 1;
 
     // Inject commands definition at the beginning of the function scope
-    const codeWithInjectedCommands = `
-        ${code.slice(0, codeScopeStart)}\n
-        ${commandsObjectLine}
-        ${code.slice(codeScopeStart)}
-    `;
+    const codeWithInjectedCommands = `${code.slice(0, codeScopeStart)}${commandsObjectLine}${code.slice(codeScopeStart)}`;
 
     // Make blob from function and pass it to worker
     const blob = new Blob([`(${codeWithInjectedCommands})()`]);
